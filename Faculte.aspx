0f1679ca3eae49e5e0c9bf71293321b468151551
AngeFlower <angefleurisseineza@gmail.com>
2021-08-23 14:10:58 +0200

    add faculte

---
+++ Faculte.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="Faculte.aspx.cs" Inherits="Faculte" %>
+
+
+<asp:Content ContentPlaceHolderID="ContentPlaceholder1" ID="PlaceHolder1" runat="server">
+  
+        <div class="page-wrapper font-poppins">
+        <div class="wrapper">
+            <div class="card card-4">
+                <div class="card-body">
+                    <h2 class="title">Page pour Faculte</h2>     
+        <div class="input-group">
+        <asp:Label cssClass="label" ID="Label1" runat="server" Text="Nom"></asp:Label>
+        <asp:TextBox cssClass="input--style-4" ID="TextBox1" runat="server"></asp:TextBox>
+        </div>
+
+        <div class="p-t-5 b-w">
+        <asp:Button cssClass="btn btn--radius-2 btn--blue" ID="Button1" runat="server" OnClick="Button1_Click" style="height: 26px" Text="Ajouter" />
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
+</asp:Content>
---
+++ Faculte.aspx.cs
@@ -0,0 +1,45 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+public partial class Faculte : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+  
+    protected void Button1_Click1(object sender, EventArgs e)
+    {
+        {
+           
+        }
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        Response.Write("<script>alert('clicked')</script>");
+        string chaine = "Data Source = DESKTOP-62FIK78\\SQLEXPRESS;initial catalog = Enseignents ;user id = ult ;password = 123456;Integrated Security=true";
+        SqlConnection n = new SqlConnection(chaine);
+        n.Open();
+        if (String.IsNullOrWhiteSpace(TextBox1.Text))
+        {
+            Response.Write("Impossible d'enregistrer un champ vide");
+        }
+        else
+        {
+            string requete = "insert into Faculte (Nomfac) values('" + TextBox1.Text + "')";
+            //string persAct = "select idPersonne from Personne where idPersonne = max(idPersonne)";
+            SqlCommand com = new SqlCommand(requete, n);
+            com.ExecuteNonQuery();
+            Response.Write("Faculte Ajoutée Avec Succes.");
+            n.Close();
+            n.Open();
+        }
+    }
+}
