7e763568780a903654df9272f9ec15c58271acdc
AngeFlower <angefleurisseineza@gmail.com>
2021-08-23 14:11:48 +0200

    add salle

---
+++ Salle.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#"  MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="Salle.aspx.cs" Inherits="Salle" %>
+
+<asp:Content ContentPlaceHolderID="ContentPlaceholder1" ID="PlaceHolder1" runat="server">
+            <div class="page-wrapper font-poppins">
+        <div class="wrapper">
+            <div class="card card-4">
+                <div class="card-body">
+                    <h2 class="title">Page pour Salle</h2>
+        <div class="input-group">
+        <asp:Label cssClass="label" ID="Label3" runat="server" Text="Code"></asp:Label>
+        <div class="rs-select2 js-select-simple select--no-search">
+        <asp:DropDownList ID="DropDownList1" cssClass="input--style-4" runat="server" AutoPostBack="True" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+        </asp:DropDownList>
+        </div>
+        </div>
+
+        <div class="input-group">
+        <asp:Label cssClass="label" ID="Label1" runat="server" Text="Nom"></asp:Label>
+        <asp:TextBox cssClass="input--style-4" ID="TextBox1" runat="server"></asp:TextBox>
+        </div>
+
+        <div class="input-group">
+        <asp:Label cssClass="label" ID="Label2" runat="server" Text="Bloc"></asp:Label>
+        <asp:TextBox cssClass="input--style-4" ID="TextBox2" runat="server"></asp:TextBox>
+        </div>
+
+        <div class="p-t-5 b-w">
+        <asp:Button cssClass="btn btn--radius-2 btn--blue" ID="Button1" runat="server" OnClick="Button1_Click" Text="Ajouter" />
+        <asp:Button cssClass="btn btn--radius-2 btn--blue" ID="Button2" runat="server" OnClick="Button2_Click" Text="Modifier" />
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
+    
+</asp:Content>
---
+++ Salle.aspx.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+public partial class Salle : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string chaine = "Data Source = DESKTOP-62FIK78\\SQLEXPRESS;initial catalog = Enseignents ;user id = ult ;password = 123456;Integrated Security=true";
+        SqlConnection n = new SqlConnection(chaine);
+
+        n.Open();
+        string requete = "";
+        if (!IsPostBack)
+        {
+            requete = "select IDsalle from Salle";
+            SqlCommand com = new SqlCommand(requete, n);
+
+            SqlDataReader dr = com.ExecuteReader();
+            DropDownList1.Items.Clear();
+            while (dr.Read())
+            {
+                DropDownList1.Items.Add(dr.GetInt32(0).ToString());
+            }
+            n.Close();
+        }
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        Response.Write("<script>alert('clicked')</script>");
+        string chaine = "Data Source = DESKTOP-62FIK78\\SQLEXPRESS;initial catalog = Enseignents ;user id = ult ;password = 123456;Integrated Security=true";
+        SqlConnection n = new SqlConnection(chaine);
+        n.Open();
+       
+            string requete = "insert into Salle (Nomsalle,Bloc) values('" + TextBox1.Text + "','" + TextBox2.Text + "')";
+            SqlCommand com = new SqlCommand(requete, n);
+            com.ExecuteNonQuery();
+            Response.Write("Salle Ajoutée Avec Succes.");
+            n.Close();
+            n.Open();
+    }
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        string chaine = "Data Source = DESKTOP-62FIK78\\SQLEXPRESS;initial catalog = Enseignents ;user id = ult ;password = 123456;Integrated Security=true";
+        SqlConnection n = new SqlConnection(chaine);
+
+        n.Open();
+        string requete = "";
+        requete = "update Salle set Nomsalle = '" + TextBox1.Text + "',Bloc ='" + TextBox2.Text + "'";
+        SqlCommand com = new SqlCommand(requete, n);
+        com.ExecuteNonQuery();
+        n.Close();
+    }
+    protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        int a = Int32.Parse(DropDownList1.SelectedItem.ToString());
+        string chaine = "Data Source = DESKTOP-62FIK78\\SQLEXPRESS;initial catalog = Enseignents ;user id = ult ;password = 123456;Integrated Security=true";
+        SqlConnection n = new SqlConnection(chaine);
+        n.Open();
+        string requete = "";
+        requete = "select Nomsalle,Bloc from Salle where Salle.IDsalle = " + a + "";
+        SqlCommand com = new SqlCommand(requete, n);
+        SqlDataReader dr = com.ExecuteReader();
+
+        if (dr.Read())
+        {
+            TextBox1.Text = dr.GetString(0);
+            TextBox2.Text = dr.GetString(1);
+        }
+        n.Close();
+    }
+}
