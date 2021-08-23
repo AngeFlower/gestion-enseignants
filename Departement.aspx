ec7b09f279296b85903d6fdb0e3e642165dae462
AngeFlower <angefleurisseineza@gmail.com>
2021-08-23 14:10:50 +0200

    add departement

---
+++ Departement.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="Departement.aspx.cs" Inherits="Departement" %>
+
+<asp:Content ContentPlaceHolderID="ContentPlaceholder1" ID="PlaceHolder1" runat="server">
+    <div class="page-wrapper font-poppins">
+        <div class="wrapper">
+            <div class="card card-4">
+                <div class="card-body">
+                    <h2 class="title">Page pour Departement</h2>
+        <div class="input-group">
+        <asp:Label cssClass="label" ID="Label3" runat="server" Text="Code"></asp:Label>
+        <div class="rs-select2 js-select-simple select--no-search">
+        <asp:DropDownList ID="DropDownList2" cssClass="input--style-4" runat="server" AutoPostBack="True" OnSelectedIndexChanged="DropDownList2_SelectedIndexChanged">
+        </asp:DropDownList>
+        </div>
+        </div>
+
+        <div class="input-group">
+        <asp:Label cssClass="label" ID="Label1" runat="server" Text="Faculte"></asp:Label>
+        <div class="rs-select2 js-select-simple select--no-search">
+        <asp:DropDownList ID="DropDownList1" cssClass="input--style-4" runat="server" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged" AutoPostBack="True">
+        </asp:DropDownList>
+        </div>
+        </div>
+
+        <div class="input-group">
+        <asp:Label cssClass="label" ID="Label2" runat="server" Text="Departement"></asp:Label>
+        <asp:TextBox cssClass="input--style-4" ID="TextBox1" runat="server"></asp:TextBox>
+        </div>
+
+        <div class="p-t-5 b-w">
+        <asp:Button ID="Button1" cssClass="btn btn--radius-2 btn--blue" runat="server" OnClick="Button1_Click" Text="Ajouter" />
+        <asp:Button ID="Button2" cssClass="btn btn--radius-2 btn--blue" runat="server" OnClick="Button2_Click" Text="Modifier" />
+        </div>
+</div>
+</div>
+</div>
+</div>
+ </asp:Content>
---
+++ Departement.aspx.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+public partial class Departement : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string chaine = "Data Source = DESKTOP-62FIK78\\SQLEXPRESS;initial catalog = Enseignents ;user id = ult ;password = 123456;Integrated Security=true";
+        SqlConnection n = new SqlConnection(chaine);
+        n.Open();
+        string requete = "";
+        requete = " select IDfac,Nomfac from Faculte";
+        SqlCommand com = new SqlCommand(requete,n);
+        SqlDataReader dr = com.ExecuteReader();
+        while (dr.Read())
+        {
+            DropDownList1.Items.Add(dr.GetInt32(0)+ " " +dr.GetString(1));
+        }
+        n.Close();
+
+        n.Open();
+        if (!IsPostBack)
+        {
+            requete = "select IDdep from Departement";
+            SqlCommand com1 = new SqlCommand(requete, n);
+
+            SqlDataReader dr1 = com1.ExecuteReader();
+            DropDownList2.Items.Clear();
+            while (dr1.Read())
+            {
+                DropDownList2.Items.Add(dr1.GetInt32(0).ToString());
+            }
+            n.Close();
+        }
+
+    }
+    protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        string chaine = "Data Source = DESKTOP-62FIK78\\SQLEXPRESS;initial catalog = Enseignents ;user id = ult ;password = 123456;Integrated Security=true";
+        SqlConnection n = new SqlConnection(chaine);
+        n.Open();
+        string dep = DropDownList1.SelectedItem.ToString();
+        string [] depart = dep.Split(' ');
+        string requete = " insert into Departement(FaculteDep,NomDep)values('"+ depart[0]+"','"+TextBox1.Text+"')";
+        SqlCommand com = new SqlCommand(requete, n);
+        com.ExecuteNonQuery();
+        n.Close();
+
+    }
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        int a = Int32.Parse(DropDownList2.SelectedItem.ToString());
+        string chaine = "Data Source = DESKTOP-62FIK78\\SQLEXPRESS;initial catalog = Enseignents ;user id = ult ;password = 123456;Integrated Security=true";
+        SqlConnection n = new SqlConnection(chaine);
+
+        n.Open();
+        string requete = "";
+        requete = "update Departement set NomDep = '" + TextBox1.Text + "' where IDdep =" + a + "";
+        SqlCommand com = new SqlCommand(requete, n);
+        com.ExecuteNonQuery();
+        n.Close();
+    }
+    protected void DropDownList2_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        int a = Int32.Parse(DropDownList2.SelectedItem.ToString());
+        string chaine = "Data Source = DESKTOP-62FIK78\\SQLEXPRESS;initial catalog = Enseignents ;user id = ult ;password = 123456;Integrated Security=true";
+        SqlConnection n = new SqlConnection(chaine);
+        n.Open();
+        string requete = "";
+        requete = "select NomDep from Departement where Departement.IDdep = " + a + "";
+        SqlCommand com = new SqlCommand(requete, n);
+        SqlDataReader dr = com.ExecuteReader();
+
+        if (dr.Read())
+        {
+            TextBox1.Text = dr.GetString(0);
+        }
+        n.Close();
+    }
+}
