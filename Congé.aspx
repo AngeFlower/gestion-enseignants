99c00f37cbdbe814c3c0887bea26bc1d19488b54
AngeFlower <angefleurisseineza@gmail.com>
2021-08-23 14:10:17 +0200

    add conge

---
+++ Congé.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="Congé.aspx.cs" Inherits="Congé" %>
+
+<asp:Content ContentPlaceHolderID="ContentPlaceholder1" ID="PlaceHolder1" runat="server">
+            <div class="page-wrapper font-poppins">
+        <div class="wrapper">
+            <div class="card card-4">
+                <div class="card-body">
+                    <h2 class="title">Page pour Congé</h2> 
+        <div class="input-group">
+        <asp:Label cssClass="label" ID="Label1" runat="server" Text="Enseignent"></asp:Label>
+        <div class="rs-select2 js-select-simple select--no-search">
+        <asp:DropDownList ID="DropDownList1" cssClass="input--style-4" runat="server">
+        </asp:DropDownList>
+        </div>
+        </div>
+
+        <div class="input-group">
+        <asp:Label cssClass="label" ID="Label2" runat="server" Text="Date_debut"></asp:Label>
+        <asp:TextBox cssClass="input--style-4" ID="TextBox1" runat="server" TextMode="Date"></asp:TextBox>
+        </div>
+
+        <div class="input-group">
+        <asp:Label cssClass="label" ID="Label3" runat="server" Text="Date_fin"></asp:Label>
+        <asp:TextBox cssClass="input--style-4" ID="TextBox2" runat="server" TextMode="Date"></asp:TextBox>
+        </div>
+        
+        <div class="p-t-5 b-w">
+        <asp:Button cssClass="btn btn--radius-2 btn--blue" ID="Button1" runat="server" OnClick="Button1_Click" Text="Ajouter" />
+        </div>
+</div>
+</div>
+</div>
+</div>
+</asp:Content>
---
+++ Congé.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.Sql;
+using System.Data.SqlClient;
+public partial class Congé : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string chaine = "Data Source = DESKTOP-62FIK78\\SQLEXPRESS;initial catalog = Enseignents ;user id = ult ;password = 123456;Integrated Security=true";
+        SqlConnection n = new SqlConnection(chaine);
+        n.Open();
+        string requete = " select IDens,Nompers,Prenompers,Degre from Enseignent,Personne where Enseignent.PersonneEns = Personne.IDpers";
+        SqlCommand com = new SqlCommand(requete, n);
+        SqlDataReader dr = com.ExecuteReader();
+        while (dr.Read())
+        {
+            DropDownList1.Items.Add(dr.GetInt32(0) + " " + dr.GetString(1) + " " + dr.GetString(2) + " " + dr.GetString(3));
+        }
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        string chaine = "Data Source = DESKTOP-62FIK78\\SQLEXPRESS;initial catalog = Enseignents ;user id = ult ;password = 123456;Integrated Security=true";
+        SqlConnection n = new SqlConnection(chaine);
+        n.Open();
+        string con = DropDownList1.SelectedItem.ToString();
+        string[] cong = con.Split(' ');
+        string requete = " insert into Congé(EnseignentCong,Debut,Fin)values('" + cong[0] + "','" + TextBox1.Text + "','" + TextBox2.Text + "')";
+        SqlCommand com = new SqlCommand(requete, n);
+        com.ExecuteNonQuery();
+        n.Close();
+    }
+}
