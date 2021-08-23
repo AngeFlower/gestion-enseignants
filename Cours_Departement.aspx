7115c6dc2e826f105c481299fd8eb9e78fc01bbe
AngeFlower <angefleurisseineza@gmail.com>
2021-08-23 14:10:41 +0200

    add cours_departement

---
+++ Cours_Departement.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="Cours_Departement.aspx.cs" Inherits="Cours_Departement" %>
+
+<asp:Content ContentPlaceHolderID="ContentPlaceholder1" ID="PlaceHolder1" runat="server">
+
+            <div class="page-wrapper font-poppins">
+        <div class="wrapper">
+            <div class="card card-4">
+                <div class="card-body">
+                    <h2 class="title">Page pour Cours_dep</h2> 
+        <div class="input-group">
+        <asp:Label cssClass="label" ID="Label1" runat="server" Text="Departement"></asp:Label>
+        <div class="rs-select2 js-select-simple select--no-search">
+        <asp:DropDownList ID="DropDownList1" cssClass="input--style-4" runat="server">
+        </asp:DropDownList>
+        </div>
+        </div>
+
+        <div class="input-group">
+        <asp:Label cssClass="label" ID="Label2" runat="server" Text="Cours"></asp:Label>
+        <div class="rs-select2 js-select-simple select--no-search">
+        <asp:DropDownList ID="DropDownList2" cssClass="input--style-4" runat="server">
+        </asp:DropDownList>
+        </div>
+        </div>
+
+        <div class="p-t-5 b-w">
+        <asp:Button cssClass="btn btn--radius-2 btn--blue" ID="Button1" runat="server" OnClick="Button1_Click1" Text="Ajouter" />
+        </div>
+</div>
+</div>
+</div>
+</div>
+</asp:Content>
---
+++ Cours_Departement.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+public partial class Cours_Departement : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string chaine = "Data Source = DESKTOP-62FIK78\\SQLEXPRESS;initial catalog = Enseignents ;user id = ult ;password = 123456;Integrated Security=true";
+        SqlConnection n = new SqlConnection(chaine);
+        n.Open();
+        string requete;
+        requete = "";
+        requete = "select IDfac,NomDep from Faculte,Departement where Departement.FaculteDep = Faculte.IDfac";
+        SqlCommand pt = new SqlCommand(requete, n);
+        SqlDataReader dr = pt.ExecuteReader();
+        while (dr.Read())
+        {
+            DropDownList1.Items.Add(dr.GetInt32(0) + " " + dr.GetString(1));
+        }
+        n.Close();
+        n.Open();
+
+        requete = "select IDcours,Nomcours from Cours";
+        SqlCommand ant = new SqlCommand(requete, n);
+        SqlDataReader dr1 = ant.ExecuteReader();
+        while (dr1.Read())
+        {
+            DropDownList2.Items.Add(dr1.GetInt32(0) + " " + dr1.GetString(1));
+        }
+        n.Close();
+    }
+
+  
+    protected void Button1_Click1(object sender, EventArgs e)
+    {
+        string chaine = "Data Source = DESKTOP-62FIK78\\SQLEXPRESS;initial catalog = Enseignents ;user id = ult ;password = 123456;Integrated Security=true";
+        SqlConnection n = new SqlConnection(chaine);
+        n.Open();
+        string dep = DropDownList1.SelectedItem.ToString();
+        string[] depart = dep.Split(' ');
+        string crs = DropDownList2.SelectedItem.ToString();
+        string[] cour = crs.Split(' ');
+        string crdep = "insert into CoursDepartement(dep,cours)values(" + depart[0] + "," + cour[0] + ")";
+        SqlCommand com = new SqlCommand(crdep, n);
+        com.ExecuteNonQuery();
+        Response.Write("<script>alert('enregistrement reussi avec success')</script>");
+    }
+}
